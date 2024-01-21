9ff730362cb662780d80c99d0cb8434c80967eb3
bkfwebdev <bryant.feld@gmail.com>
2024-01-21 13:14:06 -0500

    fixed accidental code deletion

--- longestSequence2.js
+++ longestSequence2.js
@@ -1,5 +1,27 @@
 var lengthOfLongestSubstring = function(s) {
- 
+    let pointer = 0;
+    let maxLength = 0;
+    let lengthCount = 0;
+    let foundMap = new Set();
+
+    while (pointer < s.length) {
+        const char = s.charAt(pointer);
+
+        if (!foundMap.has(char)) {
+            foundMap.add(char);
+            lengthCount++;
+            pointer++;
+        } else {
+            foundMap.delete(s.charAt(pointer - lengthCount));
+            lengthCount--;
+        }
+
+        if (lengthCount > maxLength) {
+            maxLength = lengthCount;
+        }
+    }
+
+    return maxLength;
 };
 
 let test1 = lengthOfLongestSubstring("abcabcbb");
